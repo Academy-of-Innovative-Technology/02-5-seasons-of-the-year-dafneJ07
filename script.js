225cf873abfa4f898067342009db8ca9301f4151
Dafne <dafnej@nycstudents.net>
2026-01-14 14:09:07 -0500

    complete

--- script.js
+++ script.js
@@ -10,3 +10,5 @@ var summerImages = ["https://bourbonandvinyl.net/wp-content/uploads/2023/07/8060
 var fallImages = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfM_6S5YvBgglSCgMYewWjBOd5C6PqxkwD-A&s"]
 
 var winterImages = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3DAAYQDom6dVpaJ1FGMpb9QpsCjVQ8wG8Dw&s"]
+
+//yay//
